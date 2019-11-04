158602836cfa89b6af5410f53680e8bfb2ff3717
itay <itayperry91@gmail.com>
2019-11-04 17:02:06 +0200

    load more songs button created

--- src/components/GeneralSongsList.js
+++ src/components/GeneralSongsList.js
@@ -4,24 +4,26 @@ import GeneralSongItem from './GeneralSongItem';
 const GeneralSongsList = props => {
   console.log(props.songs);
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Duration</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.songs.map((item, index) => (
-          <GeneralSongItem key={index} item={item} i={index} />
-        ))}
-      </tbody>
-      <tfoot>
-        <tr>{/* <td>Sum</td>
+    <div className='table_container'>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Duration</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.songs.map((item, index) => (
+            <GeneralSongItem key={index} item={item} i={index} />
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>{/* <td>Sum</td>
                 <td>$180</td> */}</tr>
-      </tfoot>
-    </table>
+        </tfoot>
+      </table>
+    </div>
   );
 };
 
--- src/components/users-favourites/FavouriteSongs.js
+++ src/components/users-favourites/FavouriteSongs.js
@@ -5,46 +5,70 @@ import FavouriteSong from './FavouriteSong';
 const FavouriteSongs = () => {
   const [songs, setSongs] = useState([]);
   const [accessToken] = useContext(AccessTokenContext);
+  const [nextPageUrl, setNextPageUrl] = useState(
+    `https://api.spotify.com/v1/me/tracks?offset=0&limit=10`
+  );
+  const [totalNumOfSongs, setTotalNumOfSongs] = useState(0);
 
   useEffect(() => {
-    (async function() {
-      const songsResponse = await fetch(
-        'https://api.spotify.com/v1/me/tracks?limit=13',
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`
-          }
-        }
-      );
-      if (!songsResponse.ok) {
-        const text = await songsResponse.text();
-        throw new Error(`Spotify error: ${songsResponse.status}: ${text}`);
-      }
-      const jsonResponse = await songsResponse.json();
-      setSongs(jsonResponse.items);
-      console.log(jsonResponse.items);
-    })();
+    fetchData();
   }, [accessToken]);
+
+  const fetchData = async () => {
+    const songsResponse = await fetch(nextPageUrl, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+    if (!songsResponse.ok) {
+      const text = await songsResponse.text();
+      throw new Error(`Spotify error: ${songsResponse.status}: ${text}`);
+    }
+    const jsonResponse = await songsResponse.json();
+    // You can't use set functions direcly - React will require them all to be dependencies
+    updateNextPage(jsonResponse.next);
+    updateNumOfSongs(jsonResponse.total);
+    updateSongs(jsonResponse.items);
+  };
+
+  const updateSongs = newSongs => {
+    setSongs([...songs, ...newSongs]);
+  };
+  const updateNextPage = url => {
+    setNextPageUrl(url);
+  };
+
+  const updateNumOfSongs = val => {
+    setTotalNumOfSongs(val);
+  };
 
   return (
     <div>
-      <h2>Favourite Songs</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Album</th>
-            <th>Artist</th>
-            <th>Duration</th>
-          </tr>
-        </thead>
-        <tbody>
-          {songs.map((item, index) => (
-            <FavouriteSong key={index} item={item} />
-          ))}
-        </tbody>
-        {/* <tfoot> tag - optional */}
-      </table>
+      <h2 style={{ marginTop: '1rem', marginBottom: '1rem' }}>
+        Favourite Songs : {totalNumOfSongs}
+      </h2>
+      <div className='table_container'>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Album</th>
+              <th>Artist</th>
+              <th>Duration</th>
+            </tr>
+          </thead>
+          <tbody>
+            {songs.map((item, index) => (
+              <FavouriteSong key={index} item={item} />
+            ))}
+          </tbody>
+          {/* <tfoot> tag - optional */}
+        </table>
+      </div>
+      {nextPageUrl && (
+        <button onClick={fetchData}>Change Offset (add 10 songs)</button>
+      )}
+      {/* button disappears when there are no more songs */}
     </div>
   );
 };
