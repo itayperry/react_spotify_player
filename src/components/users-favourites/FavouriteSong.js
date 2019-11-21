a5091d8f9f51c1dc0a93e73940473da3d3424b07
itay <itayperry91@gmail.com>
2019-11-21 13:37:31 +0200

    song selected css class now synced with context

--- src/components/users-favourites/FavouriteSong.js
+++ src/components/users-favourites/FavouriteSong.js
@@ -2,24 +2,16 @@ import React, { useContext, Fragment } from 'react';
 import { PlayerSourceContext } from '../../store/PlayerSourceContext';
 import { convertSongDuration } from '../../utils/utils';
 import ArtistsLinksBySong from '../ArtistsLinksBySong';
+
 const FavouriteSong = props => {
   const setPlayerSource = useContext(PlayerSourceContext)[1];
-  // console.log(props.item.track);
-  // const [playerSource, setPlayerSource] = useContext(PlayerSourceContext); will provide an unnecessary variable
-  // console.log(props.active);
+
   return (
     <tr
-      // className='song-in-table'
       className={'song-in-table ' + (props.active ? 'chosen_active_song' : '')}
-      // onClick={() => setPlayerSource(props.item.track.preview_url)}
-      onClick={() => {
-        props.changeSelection(props.idx);
-        setPlayerSource(props.item.track);
-      }}
+      onClick={() => setPlayerSource(props.item.track)}
     >
       <td>{props.item.track.name}</td>
-      {/* <td>{props.item.track.artists[0].name}</td> */}
-      {/* <td>{createArtistsString(props.item.track.artists)}</td> */}
       <td>
         <ArtistsLinksBySong artistArrOfObj={props.item.track.artists} />
       </td>
--- src/components/users-favourites/FavouriteSongs.js
+++ src/components/users-favourites/FavouriteSongs.js
@@ -2,19 +2,15 @@ import React, { useState, useContext, useEffect } from 'react';
 import { AccessTokenContext } from '../../store/AccessTokenContext';
 import FavouriteSong from './FavouriteSong';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import { PlayerSourceContext } from '../../store/PlayerSourceContext';
 
 const FavouriteSongs = () => {
+  const [playerSource] = useContext(PlayerSourceContext);
   const [songs, setSongs] = useState([]);
   const [accessToken] = useContext(AccessTokenContext);
   const [nextPageUrl, setNextPageUrl] = useState(
     `https://api.spotify.com/v1/me/tracks?offset=0&limit=20`
   );
-  const [selectedSongIndex, setSelectedSongIndex] = useState(null);
-
-  const handleToggle = index => {
-    console.log('activated');
-    setSelectedSongIndex(index);
-  };
 
   useEffect(() => {
     fetchData();
@@ -22,7 +18,6 @@ const FavouriteSongs = () => {
   }, [accessToken]);
 
   async function fetchData() {
-    console.log('triggered');
     const songsResponse = await fetch(nextPageUrl, {
       headers: {
         Authorization: `Bearer ${accessToken}`
@@ -31,11 +26,10 @@ const FavouriteSongs = () => {
     if (!songsResponse.ok) {
       const text = await songsResponse.text();
       // setError(`Spotify error: ${songsResponse.status}: ${text}`);
-      // console.log(error);
+      console.log(songsResponse.status);
       throw new Error(`Spotify error: ${songsResponse.status}: ${text}`);
     }
     const jsonResponse = await songsResponse.json();
-    console.log(jsonResponse.total);
     setNextPageUrl(jsonResponse.next);
     // setTotalNumOfSongs(jsonResponse.total);
     setSongs([...songs, ...jsonResponse.items]);
@@ -79,21 +73,14 @@ const FavouriteSongs = () => {
             <tbody>
               {songs.map((item, index) => (
                 <FavouriteSong
-                  // key={index}
-                  key={item.id}
+                  key={item.track.id}
                   item={item}
-                  changeSelection={handleToggle}
-                  active={index === selectedSongIndex}
-                  idx={index}
+                  active={item.track.id === playerSource.id}
                 />
               ))}
             </tbody>
           </table>
         </div>
-        {/* {nextPageUrl && (
-          <button onClick={fetchData}>Change Offset (add 10 songs)</button>
-      )} */}
-        {/* button disappears when there are no more songs */}
       </div>
     </InfiniteScroll>
   );
@@ -103,12 +90,24 @@ export default FavouriteSongs;
 // const [totalNumOfSongs, setTotalNumOfSongs] = useState(0);
 // const [error, setError] = useState(null);
 
+// const [selectedSongId, setSelectedSongId] = useState('');
+
+// const handleToggle = id => {
+//   console.log('activated');
+//   setSelectedSongId(id);
+// };
+
 // {songs.map((item, index) => (
 //   <FavouriteSong
 //     key={index}
 //     key={item.id}
 //     item={item}
 //     changeSelection={handleToggle}
-//     active={index === selectedSongIndex}
+//     active={index === selectedSongId}
 //     idx={index}
 //   />
+
+// {/* {nextPageUrl && (
+//   <button onClick={fetchData}>Change Offset (add 10 songs)</button>
+// )} */}
+// {/* button disappears when there are no more songs */}
